21d8c01ba34a46329a652d790df9dbf25817e253
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-01-25 11:27:45 +0100

    read file 2

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import {writeFile, readFile} from "fs/promises";
 
 console.log("Hello World");
-
 writeFile("foo.txt", "bar");
+
+
+
+// Write File
 writeFile("hello.json", JSON.stringify({x: "5", y:"10"}, null, 4  ));
+
+readFile("hello.json", "utf8")
+    .then(data => {
+       return JSON.parse(data)
+    })
+    .then(data => {
+        const newData = JSON.stringify({...data, "name": "Rick", "age": "70"})
+        return writeFile("./Hello.json", newData)
+    })
 
